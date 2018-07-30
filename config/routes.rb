48317ffbae9e70ca786e56bc67e0ddde8ff26797
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2018-07-30 10:37:16 -0300

    pre build

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do   
   resources :snippets
 
+  root to: 'snippets#stream'
+
   get 'snippet/:slug', to: 'snippets#show'
   get 'snippet/:slug/stream', to: 'snippets#stream'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
